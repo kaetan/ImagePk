14e282e55e3ffb3dcf5a1d8cfd3a18c005b2067a
Kirill Prokudin <kir.prokudin@yandex.ru>
2020-06-10 23:18:30 +0700

    + added provider

---
+++ src/Providers/ImagePkServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace ImagePk\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImagePkServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
